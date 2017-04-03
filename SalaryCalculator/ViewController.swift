3a664448a4f8c17ac2647ed549a9909b3d8ffbbe
Kyaw Lin <mgkyawlinndidi@gmail.com>
2017-04-02 23:15:33 -0700

    Initial Commit

---
+++ SalaryCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SalaryCalculator
+//
+//  Created by Kyaw Lin on 2/4/60 BE.
+//  Copyright © 2560 BE Kyaw Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
